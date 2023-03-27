9dd5276d2b40a94a1e62d88774fb9eec69c0c9e8
Ivan Mieres <mieres.iv@gmail.com>
2023-03-27 01:08:35 -0300

    ejercicio de práctica

--- src/main/java/model/Letra.java
+++ src/main/java/model/Letra.java
@@ -33,8 +33,8 @@ public class Letra implements ILetra {
     public void crearT() throws InterruptedException {
         for (int fila = 0; fila < letra.length; fila++) {
             for (int columna = 0; columna < letra[0].length; columna++) {
-                if (fila == 0) letra[fila][columna] = '*';
-                else if (fila > 0 && columna == 3) letra[fila][columna] = '*';
+                if (fila == 0) letra[fila][columna] = '-';
+                else if (fila > 0 && columna == 3) letra[fila][columna] = '|';
                 else letra[fila][columna] = ' ';
                 System.out.print(letra[fila][columna]);
                 Thread.sleep(100);
@@ -47,8 +47,8 @@ public class Letra implements ILetra {
     public void crearO() throws InterruptedException {
         for (int fila = 0; fila < letra.length; fila++) {
             for (int columna = 0; columna < letra[0].length; columna++) {
-                if (fila == 0 || fila == 3) letra[fila][columna] = '*';
-                else if (fila >= 0 && columna == 0 || fila >= 0 && columna == 6) letra[fila][columna] = '*';
+                if (fila == 0 || fila == 3) letra[fila][columna] = '-';
+                else if (fila >= 0 && columna == 0 || fila >= 0 && columna == 6) letra[fila][columna] = '|';
                 else letra[fila][columna] = ' ';
                 System.out.print(letra[fila][columna]);
                 Thread.sleep(100);
@@ -62,8 +62,8 @@ public class Letra implements ILetra {
 
         for (int fila = 0; fila < letra.length; fila++) {
             for (int columna = 0; columna < letra[0].length; columna++) {
-                if (fila == 0 || fila == 3) letra[fila][columna] = '*';
-                else if (fila >= 0 && columna == 2 || fila >= 0 && columna == 6) letra[fila][columna] = '*';
+                if (fila == 0 || fila == 3) letra[fila][columna] = '-';
+                else if (fila >= 0 && columna == 2 || fila >= 0 && columna == 6) letra[fila][columna] = '|';
                 else letra[fila][columna] = ' ';
                 System.out.print(letra[fila][columna]);
                 Thread.sleep(100);
@@ -76,8 +76,8 @@ public class Letra implements ILetra {
     public void crearC() throws InterruptedException {
         for (int fila = 0; fila < letra.length; fila++) {
             for (int columna = 0; columna < letra[0].length; columna++) {
-                if (fila == 0 || fila == 3) letra[fila][columna] = '*';
-                else if (fila >= 0 && columna == 0) letra[fila][columna] = '*';
+                if (fila == 0 || fila == 3) letra[fila][columna] = '-';
+                else if (fila >= 0 && columna == 0) letra[fila][columna] = '|';
                 else letra[fila][columna] = ' ';
                 System.out.print(letra[fila][columna]);
                 Thread.sleep(100);
@@ -90,8 +90,8 @@ public class Letra implements ILetra {
     public void crearE() throws InterruptedException {
         for (int fila = 0; fila < letraE.length; fila++) {
             for (int columna = 0; columna < letraE[0].length; columna++) {
-                if (fila == 0 || fila == 4) letraE[fila][columna] = '*';
-                else if (fila >= 0 && columna == 0 || fila == 2 && columna < 4) letraE[fila][columna] = '*';
+                if (fila == 0 || fila == 4) letraE[fila][columna] = '-';
+                else if (fila >= 0 && columna == 0 || fila == 2 && columna < 4) letraE[fila][columna] = '|';
                 else letraE[fila][columna] = ' ';
                 System.out.print(letraE[fila][columna]);
                 Thread.sleep(100);
